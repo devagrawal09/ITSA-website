8b944079b65543c29696b4092d5c46f8deae76de
Dev Agrawal <devagrawal09@gmail.com>
2021-09-19 08:41:14 -0400

    first draft created

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(function () {
+  //toggle scroll menu
+  var scrollTop = 0;
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    //adjust menu background
+    if (scroll > 80) {
+      if (scroll > scrollTop) {
+        $(".smart-scroll").addClass("scrolling").removeClass("up");
+      } else {
+        $(".smart-scroll").addClass("up");
+      }
+    } else {
+      // remove if scroll = scrollTop
+      $(".smart-scroll").removeClass("scrolling").removeClass("up");
+    }
+
+    scrollTop = scroll;
+
+    // adjust scroll to top
+    if (scroll >= 600) {
+      $(".scroll-top").addClass("active");
+    } else {
+      $(".scroll-top").removeClass("active");
+    }
+    return false;
+  });
+});
